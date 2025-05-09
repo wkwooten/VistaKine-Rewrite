8f0730fa4300a3ead80d408211eb264e2018c496
wkwooten <williamkwooten14@gmail.com>
2025-05-09 14:56:00 -0400

    Fix build errors

---
+++ src/lib/index.js
@@ -0,0 +1,4 @@
+// This is the entry point for the Svelte package
+// Export your library's components and modules here
+// For example:
+// export { default as MyComponent } from './components/MyComponent.svelte';
